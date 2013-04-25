d7a0e77948004e0b3488dc79be2c5637288fdf7f
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-04-24 22:36:46 -0600

    Better on iPhone

--- sm.js
+++ sm.js
@@ -7,7 +7,14 @@ $(document).on('ready', function () {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: new google.maps.LatLng(39.58642, -105.07710),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 12
+        zoom: 12,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+        panControl: false,
+        scaleControl: false,
+        zoomControl: false,
+        streetViewControl: false
     });
 
     var ftLayer = new google.maps.FusionTablesLayer({
@@ -15,41 +22,14 @@ $(document).on('ready', function () {
         query: $.extend({}, base_query)
     });
 
-    /*
-    var changeQuery = function () {
-        var checkedInputs = $('.sign-type:checked'),
-            options = {
-                query: $.extend({}, base_query)
-            };
-        if (checkedInputs.length === 0) {
-            options.query.where = '1=0';
-        } else if (checkedInputs.length < 3) {
-            var wheres = [];
-            for (var i = 0; i < checkedInputs.length; i++) {
-                var $input = $(checkedInputs[i]);
-                wheres.push($input.data('field') + '=1');
-            }
-            options.query.where = wheres.join(' OR ');
-            //console.log('wheres are');
-            //console.log(options.query.where);
-        }
-        console.log('options are');
-        console.log(options);
-        ftLayer.setOptions(options);
-    };
-    */
-
     var signChange = function () {
         var options = {
             query: $.extend({}, base_query)
         };
         options.query.where = $('#sign-select').val();
-        console.log('options are');
-        console.log(options);
         ftLayer.setOptions(options);
     };
 
-    //$('.sign-type').on('click', changeQuery);
     $('#sign-select')
         .on('change', signChange)
         .trigger('change');
